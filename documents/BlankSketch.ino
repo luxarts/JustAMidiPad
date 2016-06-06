04f6ee6f6c2197d03e3ed2a1416dfb24008ff536
Lucas Bacelo <bacelolucas@gmail.com>
2016-06-06 19:24:07 -0300

    Add files via upload

---
+++ documents/BlankSketch.ino
@@ -0,0 +1,23 @@
+/*  
+ *          Just A Midi Pad 
+ * www.github.com/luxarts/JustAMidiPad
+ * Module: 
+ * Version:  
+ * 
+ * Created by LuxARTS, OG-Jonii & pablonobile99
+ *               2016
+ */
+//Libraries
+
+//Prototypes
+
+//Global variables
+
+//Functions
+void setup(){
+  
+}
+
+void loop(){
+  
+}
