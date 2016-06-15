db8a6a0a06d0992c7b5e0599ac57caf24da7049a
OG-Jonii <jonikpo98@hotmail.com>
2016-06-15 03:25:36 +0000

    Add files via upload
    El codigo esta probado, funciona. Lee bien los potenciometros mediante los pines Analogicos de la placa Arduino Uno y muestra bien los datos por el monitor serie de la misma

--- modules/AD-N/AD-N.ino
+++ modules/AD-N/AD-N.ino
@@ -2,7 +2,7 @@
  *          Just A Midi Pad 
  * www.github.com/luxarts/JustAMidiPad
  * Module: AD
- * Version: 0.0
+ * Version: 1.0
  * 
  * Created by LuxARTS, OG-Jonii & pablonobile99
  *               2016
@@ -13,24 +13,34 @@
 
 //Global variables
 
+int pote_anterior[6];
+
 //Functions
 void setup(){
 
   Serial.begin(9600);
+  for(byte i=0;i<6;i++)pote_anterior[i]=analogRead(i);//Leo los pines analogicos y guardo lo que lee
 }
 
 void loop(){
 
-  int pote[6],i;
-  for(i=0;i<=5;i++)
+  int pote[6];
+  byte mapeo;
+  
+  for(byte i=0;i<6;i++)
   {
     pote[i]=analogRead(i);//Lectura del puerto analogico
-    Serial.print("POTE");
-    Serial.print(i+1);
-    Serial.print("= ");
-    Serial.print(pote[i]);//Muestro los valores guardados del puerto AD
-    Serial.println();
-    delayMicroseconds(100);//Delay minimo entre cada lectura de cada pin del AD
+    mapeo=map(pote[i],0,1023,0,127);//Mapeo entre 0 y 1023 a 0 y 127
+
+    if(pote[i]>pote_anterior[i]+2 || pote[i]<pote_anterior[i]-2)//Comparo 1er lectura con 2da lectura
+    {
+        Serial.print("POTE");
+        Serial.print(i+1);
+        Serial.print("= ");
+        Serial.print(mapeo);//Imprimo el mapeo hecho en la 2da lectura de los pines Analogicos
+        Serial.println();
+        pote_anterior[i]=pote[i];//Igualo para luego comparar con el sig
+    }
   }
 }
 
